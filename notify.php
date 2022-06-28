15e81e75f19c0363765d3923af97afb58c01836d
GeoffreyOuma <geoffreyouma@students.uonbi.ac.ke>
2022-06-28 21:07:10 +0300

    first commit

---
+++ notify.php
@@ -0,0 +1,33 @@
+<?php
+
+  $receiving_email_address = 'bygeoffreyouma@gmail.com';
+
+  if( file_exists($php_email_form = 'mail.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject ="Notify me request";
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $contact->add_message( $_POST['name'], 'Name');
+  $contact->add_message( $_POST['email'], 'Email');
+
+  echo $contact->send();
+?>
